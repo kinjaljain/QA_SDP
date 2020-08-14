d3c6a15ede82c378bcb61863c5fe8c9628269347
Kinjal <kinjal@Kinjals-MacBook-Pro.local>
2020-08-14 19:10:16 -0400

    Modify task 2 scripts

--- src/task_2/task2_prepare_traindata.py
+++ src/task_2/task2_prepare_traindata.py
@@ -4,8 +4,12 @@ import tqdm
 from collections import namedtuple
 from nltk.corpus import stopwords
 import csv
+import Levenshtein as lvstn
+from rake_nltk import Rake
+from summa import summarizer
+# from similarity_metrics.ir import rakey
 
-root_path = "/Users/kinjal/Desktop/Spring2020/11797/QA_SDP/src/dataloaders/"
+root_path = "/Users/kinjal/Desktop/Spring2020/11797/QA_SDP/src/task_2/"
 
 Datum = namedtuple('Datum', 'ref cite offsets author is_test facet year')
 Offsets = namedtuple('Offsets', 'marker cite ref')
@@ -13,12 +17,93 @@ Article = namedtuple('Article', 'id xml sentences sections')
 
 dev_ids = ["C00-2123", "C04-1089", "I05-5011", "N06-2049", "P05-1004", "P98-1046", "P98-2143", "W03-0410"]
 
+stop_words = set(stopwords.words('english'))
+rakey = Rake(max_length=1, ranking_metric=0)
+memo_id_2_score = {}
+
+def encode(sentence):
+    return sentence
+
+def has_multiple_cites(sentence):
+    matches = re.findall(r"\D(\d{4})\D", sentence)
+    return len(matches)
+
+def get_similarity_score(sentence1, sentence2):
+    tokens1 = set(re.findall(r'[\w]+', sentence1.lower()))
+    tokens2 = set(re.findall(r'[\w]+', sentence2.lower()))
+    rakey.extract_keywords_from_sentences(sentence1.replace("-", " ").lower().split())
+    keys1 = rakey.get_ranked_phrases()
+    rakey.extract_keywords_from_sentences(sentence2.replace("-", " ").lower().split())
+    keys2 = rakey.get_ranked_phrases()
+    keys2 = set(keys2)
+    tokens1 = set(keys1) - stop_words
+    tokens2 = set(keys2) - stop_words
+    return len(tokens1.intersection(tokens2)) / len(tokens1.union(tokens2))
+
+def get_id2_score(ref):
+    global memo_id_2_score
+    if ref.id in memo_id_2_score:
+        return memo_id_2_score[ref.id]
+    sents = ref.sentences.values()
+    sents_merged = '\n'.join(sents)
+    sent_scores = {x[0]: x[1] for x in summarizer.summarize(sents_merged, ratio=1, split=True, scores=True)}
+    id2score = {}
+    for id in ref.sentences:
+        sentence = ref.sentences[id]
+        if sentence in sent_scores:
+            id2score[id] = sent_scores[sentence]
+        else:
+            closest_sent = sorted([x for x in sent_scores.keys()], key=lambda x: lvstn.distance(x, sentence))[0]
+            id2score[id] = sent_scores[closest_sent]
+    memo_id_2_score[ref.id] = id2score
+    return id2score
+
+def bias_intro(s, ref_article, num_cites, id2score):
+    sid = s[0]
+    present = ("intro" in ref_article.sections[sid].lower() or "abstract" in ref_article.sections[
+        sid].lower() or "concl" in ref_article.sections[sid].lower() or "summ" in ref_article.sections[sid].lower())
+    if not present:
+        score = s[1]
+    else:
+        score = s[1]+min(0.05, (0.01)*(num_cites-1)) + 0.20 * id2score.get(sid, 0)
+        # score = s[1] + (0.02) * (num_cites - 1) + 0.3 * id2score[sid]
+    return [sid, score]
+
+def sim_score_jugaad(ref_article, similarity_score, complete_citing_sentence):
+    id2score = get_id2_score(ref_article)
+    similarity_score = {x: similarity_score[x] + 0.30 * id2score.get(x, 0) for x in similarity_score}  # 0.3
+    num_cites = has_multiple_cites(complete_citing_sentence)
+    sorted_similarity_score = sorted(similarity_score.items(), key=lambda item: -item[1])
+    top_n = [s for s in sorted_similarity_score]
+    top_n_before_filter = [bias_intro(s, ref_article, num_cites, id2score) for s in top_n]
+
+    top_n = [s for s in top_n_before_filter if s[1] > 0.15]  # 0.18
+
+    top_n = {x[0]: x[1] for x in top_n[:5]}
+    if len(top_n) == 0:
+        return {x[0]: x[1] for x in top_n_before_filter[:2]}
+    return top_n
+
+def get_best_cites(ref_article, complete_citing_sentence):
+    ref_vs = [(x[0], encode(x[1])) for i, x in enumerate(ref_article.sentences.items()) if (has_multiple_cites(x[1]) < 2)]  # or  "intro" in ref_article.sections[x[0]].lower() or "abstract" in ref_article.sections[x[0]].lower() or "concl" in ref_article.sections[x[0]].lower() or "summ" in ref_article.sections[x[0]].lower())]
+    similarity_score = {}
+    complete_citing_sentence_enc = encode(complete_citing_sentence)
+    for ref_id, ref_sentence in ref_vs:
+        try:
+            similarity_score[ref_id] = max(similarity_score.get(ref_id, 0),
+                                           get_similarity_score(ref_sentence, complete_citing_sentence_enc))
+        except Exception as e:
+            # print(e)
+            pass
+    return sim_score_jugaad(ref_article, similarity_score, complete_citing_sentence)
+
+
 # with open("%s-data-2018-clean.pkl" % root_path, 'rb') as f:
 #     dataset = pickle.load(f)
 
 with open("%sprocessed-data-2018-clean.pkl" % root_path, 'rb') as f:
     dataset = pickle.load(f)
-
+# all_results = {}
 facet_map = {}
 facet_count = {}
 facet_to_sentences = {}
@@ -32,11 +117,14 @@ for data in tqdm.tqdm(dataset):
     facets = data.facet
     ref_article = data.ref
     ref_id = data.ref.id
-    if ref_id in dev_ids:
+    if ref_id not in dev_ids:
         continue
     offsets = data.offsets
     citing_sentence_ids = offsets.cite
-    ref_sentence_ids = offsets.ref
+    full_citing_sentence = " ".join([citing_article.sentences[citing_sentence_id] for citing_sentence_id in citing_sentence_ids])
+    # ref_sentence_ids = offsets.ref
+    ref_sentences_to_score_map = get_best_cites(ref_article, full_citing_sentence)
+    ref_sentence_ids = [x for x in ref_sentences_to_score_map.keys()]
     ref_section = ref_article.sections[ref_sentence_ids[0]].lower()
     all_ref_sections.add(ref_section)
     new_ids = [x for x in citing_sentence_ids]
@@ -76,7 +164,6 @@ for facet in facet_map:
         for section_name in sorted_section_names:
             full_facet_to_ref_section_names_freq_count[facet][section_name] /= sum_for_facet
 
-stop_words = set(stopwords.words('english'))
 
 facet_word_freq = {}
 facet_word_count = {}
@@ -100,7 +187,7 @@ for facet in facet_word_freq:
         facet_word_freq[facet][word] /= facet_word_count[facet]
     sorted_facet_words = sorted(facet_word_freq[facet].items(), key=lambda x: x[1], reverse=True)
 
-with open("%straindata.csv" % root_path, "w") as f:
+with open("%stestdata.csv" % root_path, "w") as f:
     writer = csv.writer(f)
     for data in tqdm.tqdm(dataset):
         citing_article = data.cite
@@ -108,7 +195,7 @@ with open("%straindata.csv" % root_path, "w") as f:
         facets = data.facet
         ref_article = data.ref
         ref_id = data.ref.id
-        if ref_id in dev_ids:
+        if ref_id not in dev_ids:
             continue
         offsets = data.offsets
         citing_sentence_ids = offsets.cite
--- src/task_2/task2_train_rf.py
+++ src/task_2/task2_train_rf.py
@@ -1,24 +1,32 @@
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_recall_fscore_support, classification_report
 
 
-root_path = "/Users/kinjal/Desktop/Spring2020/11797/QA_SDP/src/dataloaders/"
-dataset = pd.read_csv("%straindata.csv" % root_path, header=None)
+root_path = "/Users/kinjal/Desktop/Spring2020/11797/QA_SDP/src/task_2/"
+train = pd.read_csv("%straindata.csv" % root_path, header=None)
+test = pd.read_csv("%stestdata.csv" % root_path, header=None)
 
-
-dataset.columns = ['cite_id', 'ref_id', 'cite_line_ratio', 'ref_line_ratio', 'isPercentPresent',
+train.columns = ['cite_id', 'ref_id', 'cite_line_ratio', 'ref_line_ratio', 'isPercentPresent',
                    'isFloatingPointPresent', 'facet_prob_aim', 'facet_prob_hypothesis', 'facet_prob_implication',
                    'facet_prob_method', 'facet_prob_result', 'facet_section_prob_aim', 'facet_section_prob_hypothesis',
                    'facet_section_prob_implication', 'facet_section_prob_method', 'facet_section_prob_result',
                    'is_aimcitation', 'is_hypothesiscitation', 'is_implicationcitation', 'is_methodcitation',
                    'is_resultcitation']
-print('Shape of the dataset: ' + str(dataset.shape))
-print(dataset.head())
+test.columns = ['cite_id', 'ref_id', 'cite_line_ratio', 'ref_line_ratio', 'isPercentPresent',
+                   'isFloatingPointPresent', 'facet_prob_aim', 'facet_prob_hypothesis', 'facet_prob_implication',
+                   'facet_prob_method', 'facet_prob_result', 'facet_section_prob_aim', 'facet_section_prob_hypothesis',
+                   'facet_section_prob_implication', 'facet_section_prob_method', 'facet_section_prob_result',
+                   'is_aimcitation', 'is_hypothesiscitation', 'is_implicationcitation', 'is_methodcitation',
+                   'is_resultcitation']
+# print('Shape of the dataset: ' + str(dataset.shape))
+# print(dataset.head())
 
-train, test = train_test_split(dataset, test_size=0.2)
+# train, test = train_test_split(dataset, test_size=0.2)
 x_train = train.iloc[:, 2:16]
 y_train = train.iloc[:, 16:]
 x_test = test.iloc[:, 2:16]
@@ -28,16 +36,44 @@ scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-rfc = RandomForestClassifier(bootstrap=True, max_depth=10, max_features='sqrt', random_state=1)
-rfc.fit(x_train, y_train)
+# rfc = RandomForestClassifier(n_estimators=100, bootstrap=True, max_depth=9, max_features='sqrt',
+#                              random_state=1, class_weight="balanced")
+                             # class_weight=[{0: 613/(613-51), 1: 613/51}, {0: 613/(613-9), 1: 613/9},
+                             #               {0: 613/(613-39), 1: 613/39}, {0: 613/(613-426), 1: 613/426},
+                             #               {0: 613/(613-88), 1: 613/88}])
 
-y_pred = rfc.predict(x_test)
+# clf = LogisticRegression(random_state=0).fit(x_train, y_train)
+clf = OneVsRestClassifier(LogisticRegression(multi_class='ovr', max_iter=1000, solver='lbfgs'))
+clf.fit(x_train, y_train)
+
+#
+# self.classifier.predict(test_data)
+# rfc.fit(x_train, y_train)
+
+
+# y_pred_train = rfc.predict(x_train)
+y_pred_train = clf.predict(x_train)
+
+y_train = [train.iloc[i, 16:].tolist() for i in range(0, len(x_train))]
+# print((y_pred_train == y_train))
+# num_correct = (y_pred_train == y_train)
+# print(len(x_train))
+# print("Test Accuracy : ", num_correct / len(x_train))
+print("Classification Report: ", classification_report(y_train, y_pred_train))
+F1metrics = precision_recall_fscore_support(y_train, y_pred_train, average='macro')
+print('MACRO F1score:', F1metrics[2])
+F1metrics = precision_recall_fscore_support(y_train, y_pred_train, average='micro')
+print('MICRO F1score:', F1metrics[2])
+
+
+# y_pred = rfc.predict(x_test)
+y_pred = clf.predict(x_test)
 
 # print(len(y_pred), y_pred)
 # print(len(y_test), y_test)
-
-num_correct = (y_pred == y_test).sum().item()
-print("Test Accuracy : ", num_correct / len(x_test))
+# print(len(x_test))
+# num_correct = (y_pred == y_test).sum().item()
+# print("Test Accuracy : ", num_correct / len(x_test))
 print("Classification Report: ", classification_report(y_test, y_pred))
 F1metrics = precision_recall_fscore_support(y_test, y_pred, average='macro')
 print('MACRO F1score:', F1metrics[2])
